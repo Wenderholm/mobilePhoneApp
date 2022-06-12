0ffac6e7a276d6b35609fa9219d04adfa37a9f65
wenderholm <pwenderholm@gmail.com>
2022-06-12 13:57:50 +0200

    dodawanie aplikacji za pomoca listy

--- src/com/pwenderholm/Main.java
+++ src/com/pwenderholm/Main.java
@@ -1,5 +1,8 @@
 package com.pwenderholm;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -8,9 +11,17 @@ public class Main {
         nokia.installAnApp("messenger","1.0.0","google.app", 50.0);
         nokia.installAnApp("Apach","1.0.1","google.app", 20.0);
         nokia.installAnApp("Netflix","1.0.1","google.app", 10.0);
-        nokia.installAnApp("youtube","1.2.0","google.app", 110.0);
+//        nokia.installAnApp("youtube","1.2.0","google.app", 110.0);
 
 
+//        instalowanie aplikacji
+
+        List<String> appNames = new ArrayList<>();
+        appNames.add("messenger");
+        appNames.add("facebook");
+        appNames.add("cyberbug 2077");
+
+        nokia.installAnApp(appNames);
         nokia.showAllApp();
     }
 }
